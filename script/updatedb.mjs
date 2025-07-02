0c72d63233365548593f6291b46e696a5f6be823
Pascal Rigaux <pascal.rigaux@univ-paris1.fr>
2025-07-02 10:30:44 +0200

    feat: handle env https_proxy (script/updatedb.mjs)
    ref: https://github.com/sapics/ip-location-api/issues/28

--- script/updatedb.mjs
+++ script/updatedb.mjs
@@ -1,4 +1,11 @@
 
 import {update} from '../src/db.mjs'
+import { env } from "process";
+import { ProxyAgent, setGlobalDispatcher } from 'undici';
+
+if (env.https_proxy) {
+    // needed for Node.js < 24 which do not have "NODE_USE_ENV_PROXY" (cf https://github.com/nodejs/node/pull/57165 )
+    setGlobalDispatcher(new ProxyAgent(env.https_proxy));
+}
 
 update()
